ea11b6021837ab34effea970ec8cd0061cdeada0
tammarat <tammarat.m123@gmail.com>
2025-08-28 21:25:18 +0700

    before update cli

--- src/app/layout/shell/shell.ts
+++ src/app/layout/shell/shell.ts
@@ -1,13 +1,26 @@
-import { NgClass } from '@angular/common';
+import { NgClass, CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
+import { MatIconModule } from '@angular/material/icon';
+
+export interface MenuItem {
+  name: string;
+  icon: string;
+  path: string;
+}
 @Component({
   selector: 'app-shell',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, NgClass],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, NgClass, MatIconModule],
   templateUrl: './shell.html',
 })
 export class Shell {
+  menuList: MenuItem[] = [
+    { name: 'หน้าแรก', icon: 'home', path: '/home' },
+    { name: 'เกี่ยวกับ', icon: 'person', path: '/about' },
+    { name: 'รางวัล', icon: 'work', path: '/project' },
+    { name: 'ผลงาน', icon: 'work', path: '/project' },
+  ];
   sidebarOpen = signal(false);
 
   toggleSidebar() {
